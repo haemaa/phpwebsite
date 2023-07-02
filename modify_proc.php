426e78708f29cf5b3a93e4bb3e3f9ac2bc9f08f9
haemaa <135725210+haemaa@users.noreply.github.com>
2023-07-02 16:14:19 +0900

    Update modify_proc.php

--- modify_proc.php
+++ modify_proc.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+if(!isset($_SESSION['id'])){
+  header('Location: ./test.php');
+}
 
 $con = mysqli_connect('localhost','root','1234','test');
 
@@ -10,12 +13,35 @@ $bno = $_GET['number'];
 $username = $_SESSION['id'];
 
 //post로 전달받은 부분
-$userpw = $_POST['bPassword'];
 $title = $_POST['bTitle'];
 $content = $_POST['bContent'];
 
+//파일 관련 변수
+if($_SERVER['REQUEST_METHOD'] == 'POST' && $_FILES['board_file']['name'] !=""){
+  // $filesize=$_FILES['board_file']['size'];
+  // $file_error=$_FILES['board_file']['error'];
+  $tmpfile = $_FILES['board_file']['tmp_name'];
+  $origin_filename = $_FILES['board_file']['name'];
+  $allowed_mime_types =['image/jpeg','image/png','image/gif','application/zip','application/x-hwp','application/msword','application/pdf']; //mime허락된 형식
+  $filename = iconv("UTF-8","EUC-KR",$_FILES['board_file']['name']);
+  
+  $timestamp = time();
+  $new_filename = $timestamp.'_'.$filename;
+      $file_mime_type = mime_content_type($tmpfile);
+  
+      // if(in_array($file_mime_type, $allowed_mime_types)){
+          //파일 저장 경로
+          $upload_path = './upload/'.$new_filename;
+  
+          if(move_uploaded_file($tmpfile,$upload_path)){
+              $_SESSION['write_error'] = '파일 업로드 success!';
+          } else {
+              $_SESSION['write_error'] = '파일 업로드 Fail ㅜㅜ';
+          } 
+      };
+
 //쿼리문 실행
-$query = "UPDATE board SET name='$username',password='$userpw',title='$title',content='$content' WHERE number='$bno'";
+$query = "UPDATE board SET name='$username',file='$new_filename',title='$title',content='$content' WHERE number='$bno'";
 $result = $con->query($query);
 
 echo "수정이 완료되었습니다!<br><br>";
