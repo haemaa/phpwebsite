333dfe7175b709bb11b62b49e25c26a6a33a2af6
haemaa <135725210+haemaa@users.noreply.github.com>
2023-07-02 16:15:23 +0900

    Update main.php

--- main.php
+++ main.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <?php
 session_start();
+if(!isset($_SESSION['id'])){
+  header('Location: ./test.php');
+}
 ?>
 
 <style>
@@ -14,7 +17,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Normaltic login hw</title>
+    <title>Haemaa Web</title>
     <!-- bootstrap 적용 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
